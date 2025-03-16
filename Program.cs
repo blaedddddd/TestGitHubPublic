1b4619a4b0c4a10a0ce122a710cf466659b5c303
Ellis Murphy <vladblaed@gmail.com>
2025-03-16 18:02:44 +0300

    Modernized code

--- Program.cs
+++ Program.cs
@@ -1,8 +1,1 @@
-﻿public class Program
-{
-    public static void Main()
-    {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-    }
-}
+﻿Console.WriteLine("Hello, World!");
